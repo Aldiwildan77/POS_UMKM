d7ede8cd42e6f6818b4aea33bbad9dd7b859753d
amelrnt <ochaarn@gmail.com>
2021-04-06 18:04:51 +0700

    add transaction page

--- resources/views/cashier/home.blade.php
+++ resources/views/cashier/home.blade.php
@@ -6,8 +6,9 @@
         <title></title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="{{URL::asset('https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css')}}" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-        <link rel="stylesheet" href="{{URL::asset('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css')}}" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
+        <link rel="stylesheet" href="{{URL::asset('assets1/css/style.css')}}">
+        
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
     </head>
     
     <body>
@@ -39,85 +40,102 @@
             </ul>
             <form class="form-inline my-2 my-lg-0">
                 <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                <!-- <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button> -->
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
             </form>
         </div>
     </nav>
 
-    <div class="container my-4 ">
+    <div class="container my-4">
         <div class="row">
             <div class="col-9">
-                <div class="row">
+                <div class="row my-4">
                     <!-- loop menu start here -->
                     <div class="col-4">
-                        <div class="card" >
-                            <img src="..." class="card-img-top" alt="menu 1">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-                                    Order Now
-                                </button>
-
-                                <!-- Modal -->
-                                <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                    <div class="modal-dialog" role="document">
-                                        <div class="modal-content">
-                                        <div class="modal-header">
-                                            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                            <span aria-hidden="true">&times;</span>
-                                            </button>
-                                        </div>
-                                        <div class="modal-body">
-                                            ...
-                                        </div>
-                                        <div class="modal-footer">
-                                            <button>
-                                                <i class="fas fa-plus-circle"></i>
-                                            </button>
-                                            <div class="form-group col-md-2">
-                                                <input type="text" class="form-control" id="inputZip">
-                                            </div>
-                                            <i class="fas fa-minus-circle"></i>
-                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                                        </div>
-                                        </div>
-                                    </div>
+                        <div class="card stretched-link">
+                            <div class="media">
+                                <div class="media-body p-1">
+                                    <h5 class="card-title stretched-link">Pork Chop Dinner</h5>
+                                    <h6 class="card-subtitle mb-2 text-muted">50 pcs</h6>
+                                    <h6 class="card-subtitle mb-2 font-weight-bold text-price" >Rp 25.000</h6>
                                 </div>
-
+                                <img class="img-menu align-self-end ml-3" src="https://images.unsplash.com/photo-1476224203421-9ac39bcb3327?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTN8fHxlbnwwfHx8&auto=format&fit=crop&w=700&q=60" alt="Generic placeholder image" >
                             </div>
                         </div>
                     </div>
                     <!-- loop menu end here -->
                     <div class="col-4">
-                         <!-- loop menu start here -->
-                        <div class="card" >
-                            <img src="..." class="card-img-top" alt="menu 1">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <div class="card">
+                            <div class="media">
+                                <div class="media-body p-1">
+                                    <h5 class="card-title">Pork Chop Dinner</h5>
+                                    <h6 class="card-subtitle mb-2 text-muted">50 pcs</h6>
+                                    <h6 class="card-subtitle mb-2 font-weight-bold text-price" >Rp 25.000</h6>
+                                </div>
+                                <img class="img-menu align-self-end ml-3" src="https://images.unsplash.com/photo-1473093295043-cdd812d0e601?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTF8fHxlbnwwfHx8&auto=format&fit=crop&w=700&q=60" alt="Generic placeholder image" >
                             </div>
-                        </div>
-                        <!-- loop menu end here -->
+                        </div> 
                     </div>
                     <div class="col-4">
-                         <!-- loop menu start here -->
                         <div class="card">
-                            <img src="..." class="card-img-top" alt="menu 1">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            <div class="media">
+                                <div class="media-body p-1">
+                                    <h5 class="card-title">Pork Chop Dinner</h5>
+                                    <h6 class="card-subtitle mb-2 text-muted">50 pcs</h6>
+                                    <h6 class="card-subtitle mb-2 font-weight-bold text-price" >Rp 25.000</h6>
+                                </div>
+                                <img class="img-menu align-self-end ml-3" src="https://images.unsplash.com/photo-1432139555190-58524dae6a55?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mzh8fHxlbnwwfHx8&auto=format&fit=crop&w=700&q=60" alt="Generic placeholder image" >
                             </div>
                         </div>
-                        <!-- loop menu end here -->
+                    </div>
+                </div>
+
+                <div class="row">
+                    <!-- loop menu start here -->
+                    <div class="col-4">
+                        <div class="card">
+                            <div class="media">
+                                <div class="media-body p-1">
+                                    <h5 class="card-title">Pork Chop Dinner</h5>
+                                    <h6 class="card-subtitle mb-2 text-muted">50 pcs</h6>
+                                    <h6 class="card-subtitle mb-2 font-weight-bold text-price" >Rp 25.000</h6>
+                                </div>
+                                <img class="img-menu align-self-end ml-3" src="https://images.unsplash.com/photo-1476224203421-9ac39bcb3327?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTN8fHxlbnwwfHx8&auto=format&fit=crop&w=700&q=60" alt="Generic placeholder image" >
+                            </div>
+                        </div>
+                    </div>
+                    <!-- loop menu end here -->
+                    <div class="col-4">
+                        <div class="card">
+                            <div class="media">
+                                <div class="media-body p-1">
+                                    <h5 class="card-title">Pork Chop Dinner</h5>
+                                    <h6 class="card-subtitle mb-2 text-muted">50 pcs</h6>
+                                    <h6 class="card-subtitle mb-2 font-weight-bold text-price" >Rp 25.000</h6>
+                                </div>
+                                <img class="img-menu align-self-end ml-3" src="https://images.unsplash.com/photo-1473093295043-cdd812d0e601?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTF8fHxlbnwwfHx8&auto=format&fit=crop&w=700&q=60" alt="Generic placeholder image" >
+                            </div>
+                        </div> 
+                    </div>
+                    <div class="col-4">
+                        <div class="card">
+                            <div class="media">
+                                <div class="media-body p-1">
+                                    <h5 class="card-title">Pork Chop Dinner</h5>
+                                    <h6 class="card-subtitle mb-2 text-muted">50 pcs</h6>
+                                    <h6 class="card-subtitle mb-2 font-weight-bold text-price" >Rp 25.000</h6>
+                                </div>
+                                <img class="img-menu align-self-end ml-3" src="https://images.unsplash.com/photo-1432139555190-58524dae6a55?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mzh8fHxlbnwwfHx8&auto=format&fit=crop&w=700&q=60" alt="Generic placeholder image" >
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
 
-            <div class="col-3 bg-secondary">
+            <div class="col-3">
+                <div class="row">
+                    <h5 class="mr-5"> Current Order</h5>
+                    <button class="btn btn-danger ml-5">clear</button>
+                </div>
                 <div class="row">
                     <div class="col-3">
                         Menu
@@ -137,9 +155,9 @@
     </div>
      
     
-    <script src="{{URL::asset('https://code.jquery.com/jquery-3.2.1.slim.min.js')}}" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="{{URL::asset('https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js')}}" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="{{URL::asset('https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js')}}" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
     </body>
 </html>
