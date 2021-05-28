d374d91db41e7f53a713929e156e6d5ef420130b
amelrnt <ochaarn@gmail.com>
2021-05-28 21:32:12 +0700

    fixing typos

--- routes/web.php
+++ routes/web.php
@@ -49,7 +49,7 @@ Route::get('/karyawanData', 'KaryawanController@showAll')->middleware('AuthCheck
 Route::post('/karyawanData', 'KaryawanController@addData');
 Route::post('/karyawanData/{id}', 'KaryawanController@editData');
 
-Route::get('/operasionalDlaporanAllata', 'OperasionalController@showAll')->middleware('AuthCheck');
+Route::get('/operasionalData', 'OperasionalController@showAll')->middleware('AuthCheck');
 Route::post('/operasionalData', 'OperasionalController@addData');
 Route::post('/operasionalData/{id}', 'OperasionalController@editData');
 
